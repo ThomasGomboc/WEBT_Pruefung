e2cfa667f3498039c7489672b5d915786bb0e538
Thomas <tgomboc@gmx.at>
2018-01-21 17:05:10 +0100

    probiert material funktion hinzuzufügen

--- src/BallListing.php
+++ src/BallListing.php
@@ -13,18 +13,48 @@ class BallListing
 {
     private $balllist;
 
+    public function getListMaterial()
+    {
+        return $this->balllist->material;
+    }
+
     public function __construct(Ball $ball1, Ball $ball2, Ball $ball3)
     {
-        $this->balllist[] = $ball1.$ball2.$ball3;
+        $this->balllist[] = $ball1 . $ball2 . $ball3;
     }
 
     public function __toString()
     {
-        foreach($this->balllist as $auszugebenderBall){
+        /* Hat leider nicht funktioniert
+        if (isset ($_GET['material'])) {
+            $balllistspezial = [];
+            foreach ($this->balllist as $ball) {
+                if (($_GET['material']) == $ball->getMaterial()) {
+                    $balllistspezial = $ball;
+                }
+            }
+            foreach ($balllistspezial as $ball) {
+
+                if(isset ($_GET['format'])){
+                    if($_GET['format'] = 'html') {
+                        echo($ball);
+                    } elseif($_GET['format'] = 'json'){
+                        json_encode($ball);
+                        echo($ball);
+                    }
+                } else {
+                    echo ('kein Format gegebwn');
+                }
+            }
+
+
+        } else { */
+        foreach ($this->balllist as $auszugebenderBall) {
 
             echo $auszugebenderBall;    //Einfügen eines zeilenumbruchs nicht möglich.
 
         }
-        return 'Alle Elemente der Ball liste wurden ausgegeben'."<br>";
+        return 'Alle Elemente der Ball liste wurden ausgegeben' . "<br>";
     }
+
 }
